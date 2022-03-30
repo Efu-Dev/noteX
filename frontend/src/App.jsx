ad935431794a8c9f2c261bda78e2484b1bf3f853
Diego <onlyforcodingefu@gmail.com>
2022-03-30 16:16:17 -0400

    Added API call to receive token

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from '@apollo/client';
+import {setContext} from 'apollo-link-context';
 import Header from './components/Header/header';
 import Pages from './components/index';
 
 const App = () => {
   const uri = "http://localhost:4000/api";
   const cache = new InMemoryCache();
+  const link = createHttpLink({uri});
+
+  const authLink = setContext((_, { headers }) => 
+    ({ headers: {
+        ...headers,
+        authorization: localStorage.getItem('token') || ''
+      }
+    })
+   );
   
   const client = new ApolloClient({
-    uri,
+    link: authLink.concat(link),
     cache,
+    resolvers: {},
     connectToDevTools: true
   });
 
--- frontend/src/components/SignUp/SignUp.jsx
+++ frontend/src/components/SignUp/SignUp.jsx
@@ -3,11 +3,27 @@ import {Paper, TextField, FormControl, FilledInput, InputLabel,
         InputAdornment, IconButton, Button, Typography} from '@material-ui/core';
 import {VisibilityOff, Visibility} from '@material-ui/icons';
 import useStyles from './styles';
+import {SIGNUP_USER} from '../../constants/queries';
+import { useMutation } from '@apollo/client';
+import {useNavigate} from 'react-router-dom';
 
 const SignUp = () => {
 
     const [values, setValues] = useState({username: '', password: '', email: '', showPassword: false});
     const classes = useStyles();
+    const navigate = useNavigate();
+
+    const [signUp, {loading, error}] = useMutation(SIGNUP_USER, {
+        onCompleted: (data) => {
+          localStorage.setItem('token', data.signUp);
+          navigate.push('/');
+        }
+    });
+
+    const onSubmit = (event) => {
+        event.preventDefault();
+        signUp({variables: {...values}});
+    };
 
     useEffect(() => {
         document.title = "NoteX - Sign Up";
@@ -27,42 +43,44 @@ const SignUp = () => {
     return (
         <Paper className={classes.paper}>
             <Typography variant='h2' align='center'>SignUp</Typography>
-            <FormControl fullWidth>
-                <TextField id="username" label="Username" variant="filled" className={classes.input}
-                    value={values.username} onChange={handleChange('username')} fullWidth
-                />
-
-                <TextField id="email" label="Email" variant="filled" className={classes.input}
-                    value={values.email} onChange={handleChange('email')} fullWidth
-                />
-
-                <FormControl fullWidth variant='filled' className={classes.input}>
-                    <FilledInput
-                        id="password"
-                        variant="filled"
-                        type={values.showPassword ? 'text' : 'password'}
-                        value={values.password}
-                        onChange={handleChange('password')}
-                        fullWidth
-                        endAdornment={
-                            <InputAdornment position="end">
-                                <IconButton
-                                    aria-label="toggle password visibility"
-                                    onClick={handleClickShowPassword}
-                                    onMouseDown={(event) => {event.preventDefault()}}
-                                    edge="end"
-                                >
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                                </IconButton>
-                            </InputAdornment>
-                        }
-                        label="Password"
+            <form onSubmit={e => onSubmit(e)}>
+                <FormControl fullWidth>
+                    <TextField id="username" label="Username" variant="filled" className={classes.input}
+                        value={values.username} onChange={handleChange('username')} fullWidth
                     />
-                    <InputLabel htmlFor="password">Password</InputLabel>
-                </FormControl>
 
-                <Button color='primary' variant='contained' fullWidth type='submit'>Submit</Button>
-            </FormControl>            
+                    <TextField id="email" label="Email" variant="filled" className={classes.input}
+                        value={values.email} onChange={handleChange('email')} fullWidth
+                    />
+
+                    <FormControl fullWidth variant='filled' className={classes.input}>
+                        <FilledInput
+                            id="password"
+                            variant="filled"
+                            type={values.showPassword ? 'text' : 'password'}
+                            value={values.password}
+                            onChange={handleChange('password')}
+                            fullWidth
+                            endAdornment={
+                                <InputAdornment position="end">
+                                    <IconButton
+                                        aria-label="toggle password visibility"
+                                        onClick={handleClickShowPassword}
+                                        onMouseDown={(event) => {event.preventDefault()}}
+                                        edge="end"
+                                    >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                    </IconButton>
+                                </InputAdornment>
+                            }
+                            label="Password"
+                        />
+                        <InputLabel htmlFor="password">Password</InputLabel>
+                    </FormControl>
+
+                    <Button color='primary' variant='contained' fullWidth type='submit'>Submit</Button>
+                </FormControl>  
+            </form>       
         </Paper>
         
     )
